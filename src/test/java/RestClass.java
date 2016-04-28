316e399fb80f2bfa9f9ee2eefad76a254d682d4e
Andrzej.Wudara <andrzej.wudara@dsv.com>
2016-04-28 14:17:37 +0200

    new tests

--- src/test/java/RestClass.java
+++ src/test/java/RestClass.java
@@ -1,9 +1,10 @@
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+import com.google.gson.*;
 import org.apache.commons.io.FileUtils;
+import org.json.JSONString;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -28,6 +29,46 @@ public class RestClass {
         jsonObject = new JsonParser().parse(json).getAsJsonObject();
     }
 
+
+
+
+    @Test
+    /*
+    Generate a new json that will be equal to the given one except for the title, that will be „CREATING NEW JSON” for each of the items.*//*
+    */
+    public void checkProfileImage() throws ParseException {
+
+        String newJson = "{\"items\": [\n";
+
+        JsonArray arr = jsonObject.getAsJsonArray("items");
+        for (int i = 0; i < arr.size(); i++) {
+            JsonElement element = arr.get(i).getAsJsonObject();
+            //System.out.println(element.toString());
+            JsonObject jo = element.getAsJsonObject();
+            jo.addProperty("title", "Creating new JSON");
+            jo.remove("last_activity_date");
+        }
+
+        newJson += "],";
+        jsonObject.getAsJsonPrimitive("has_more");
+        newJson += "\n \"has_more\":";
+        newJson += "\n \"quota_max\":";
+        newJson += "\n \"quota_remaining\":";
+        newJson += "\n  }";
+
+        System.out.println(newJson);
+
+    }
+
+
+//    ],
+//            "has_more": true,
+//            "quota_max": 300,
+//            "quota_remaining": 299
+
+
+
+
 //    @Test
      //Each owner has a profile_image and link values: verify that these urls are valid
 //    public void checkProfileImage(){
@@ -40,8 +81,10 @@ public class RestClass {
 //        }
 //    }
 
-    @Test
-    //Retrieve all tags and save them to a list (duplicates should be removed)
+//    @Test
+/*
+    Retrieve all tags and save them to a list (duplicates should be removed)
+*/
     public void removeTagsDuplicates(){
 
         List<String> tags = new ArrayList<String>();;
